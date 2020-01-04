9aa5bbdddba8aaad617812653fd86fd877152564
pengq <994004869@qq.com>
2020-01-04 13:21:26 +0800

    初始化项目

---
+++ src/main/java/org/bmth/mybatis/MybatisApplication.java
@@ -0,0 +1,10 @@
+package org.bmth.mybatis;
+
+/**
+ * Created by pengq on 2020/1/4 13:15.
+ */
+public class MybatisApplication {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
